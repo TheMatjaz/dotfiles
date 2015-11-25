9165b35d9e89ac592713322921ff898d8a0caa24
Matjaž <dev@matjaz.it>
2015-11-25 21:43:44 +0100

    Performs symlinking by calling a function and passing the paths of files

--- matjaz_dotfiles_installer.sh
+++ matjaz_dotfiles_installer.sh
@@ -41,75 +41,56 @@ Please try running this script again."
     }
 fi
 
-# Pick configuration files to symlink, ignore makrdown, shell scripts,
-# htop configuration file and midnight commander (mc) configuration file.
-# The last two are needed since their position is not the same on different OS.
-symlinkables=$(ls -1 | egrep --invert-match --regexp='(^.+\.(sh|md)$)|(^mc_.+$)|(^htoprc$)')
 # Install a set of basic packages on newly set systems, along with Oh My ZSH!
 bash new_system_packages_installer.sh
 
-# Create symlinks in the home directory that point to the files in the
-# dotfiles repository. Backup any existing dotfile.
-for file in $symlinkables; do
-    if [[ -e ~/.$file && ! -L ~/.$file ]]; then
-        echo "Backing up existing $file into $backup_dir"
-        mv ~/.$file $backup_dir
+# Creates a symbolic link to the file specified in the first argument $1
+# pointing to the file specified in the second argument $2. Backups any existing
+# file at the position $2 to the backup directory.
+function symlink_dotfile() {
+    local file_in_repo=$dotfiles_dir$1
+    local file_in_home=$2
+    if [[ -e $file_in_home && ! -L $file_in_home ]]; then
+        echo "Backing up existing $file_in_home into $backup_dir"
+        mv $file_in_home $backup_dir
+    #elif [ ! -e $file_in_home ]; them
+    #     touch_with_create_path($file_in_home)
     fi
-    ln -s -v -f -F $dotfiles_dir/$file ~/.$file
-done
+    ln -s -v -f -F $file_in_repo $file_in_home
+}
 
-# Symlink the htop configuration file aswell, but place it in ~/.htoprc on Macs
-# and in ~/.config/htop/htoprc on Debian
-file="htoprc"
+# Create symlinks in the home directory that point to the files in the
+# dotfiles repository. Any existing dotfiles get backupped.
+echo "Symlinking..."
+symlink_dotfile gitconfig ~/.gitconfig
+symlink_dotfile gitignore_global ~/.gitignore_global
+symlink_dotfile hgrc ~/.hgrc
+symlink_dotfile screenrc ~/.screenrc
+symlink_dotfile wgetrc ~/.wgetrc
+symlink_dotfile zsh_fino_custom.zsh-theme ~/.oh-my-zsh/custom/themes/zsh_fino_custom.zsh-theme
+symlink_dotfile zsh_aliases ~/.zsh_aliases
+symlink_dotfile zsh_path ~/.zsh_path
+symlink_dotfile zshrc ~/.zshrc
+symlink_dotfile mc_ini ~/.config/mc/ini
+symlink_dotfile mc_panels.ini ~/.config/mc/panels.ini
+symlink_dotfile emacs_init.el ~/.emacs.d/init.el
+
+# Symlink the htop configuration file as well, but place it in ~/.htoprc on Macs
+# and in ~/.config/htop/htoprc on Linux.
 case $(uname) in
     'Darwin')
-        if [[ -e ~/.$file && ! -L ~/.$file ]]; then
-            echo "Backing up existing $file into $backup_dir"
-            mv ~/.$file $backup_dir
-        fi
-        ln -s -v -f -F $dotfiles_dir/$file ~/.$file
+        symlink_dotfile htoprc ~/.htoprc
         ;;
     'Linux')
-        if [[ -e ~/.config/htop/$file && ! -L ~/.config/htop/$file ]]; then
-            echo "Backing up existing $file into $backup_dir"
-            mv ~/.config/htop/$file $backup_dir
-        fi
-        ln -s -v -f -F $dotfiles_dir/$file ~/.config/htop/$file
+        symlink_dotfile htoprc ~/.config/htop/htoprc
         ;;
     *)
         echo 'Cannot symlink htoprc on proper position on this operative system. Please update this script or perform the symlink yourself.'
     ;;
 esac
 
-# Symlink the mc configuration files into ~/.config/mc/, since it's the default
-# path for mc conficurations
-symlinkables="mc_ini mc_panels.ini"
-for file in $symlinkables; do
-    file=$(echo $file | sed 's/mc_//')
-    if [[ -e ~/.config/mc/$file && ! -L ~/.config/mc/$file ]]; then
-        echo "Backing up existing $file into $backup_dir"
-        mv ~/.config/mc/$file $backup_dir
-    fi
-    ln -s -v -f -F $dotfiles_dir/$file ~/.config/mc/$file
-done
 
-# Symlink the custom Oh My ZSH! theme
-file="zsh_fino_custom.zsh-theme"
-if [[ -e ~/.oh-my-zsh/custom/themes/$file && ! -L ~/.oh-my-zsh/custom/themes/$file ]]; then
-    echo "Backing up existing $file into $backup_dir"
-    mv ~/.oh-my-zsh/custom/themes/$file $backup_dir
-fi
-ln -s -v -f -F $dotfiles_dir/$file ~/.oh-my-zsh/custom/themes/$file
 
-# Symlink the Emacs init.el configuration file
-file="init.el"
-if [[ -e ~/.emacs.d/$file && ! -L ~/.emacs.d/$file ]]; then
-    echo "Backing up existing $file into $backup_dir"
-    mv ~/.emacs.d$file $backup_dir
-fi
-ln -s -v -f -F $dotfiles_dir/$file ~/.emacs.d/$file
 
-unset file
-unset symlinkables
 unset backup_dir
 unset dotfiles_dir
