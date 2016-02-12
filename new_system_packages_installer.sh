447150a4fc1c119dbae8c8e698cd7587b4441759
Matjaž <dev@matjaz.it>
2016-02-12 16:38:53 +0100

    Adapt new system package installer for Debian or Ubuntu
    - Remove any OS X specific part
    - Update copyright and header explanation
    - Add installer prompt
    - Add OS check at start
    - Remove apt-get autoremove, clean and autoclean

--- new_system_packages_installer.sh
+++ new_system_packages_installer.sh
@@ -1,93 +1,73 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------
-# Matjaž's dotfiles required packages installer script
+# Matjaž's dotfiles required packages installer script for Debian or Ubuntu
 #
 # >> LICENSE
-# Copyright (c) 2015, Matjaž Guštin <dev@matjaz.it> matjaz.it
+# Copyright (c) 2015-2016, Matjaž Guštin <dev@matjaz.it> matjaz.it
 # This Source Code Form is subject to the terms of the BSD 3-clause license. 
 # If a copy of the license was not distributed with this file, You can obtain
 # one at http://directory.fsf.org/wiki/License:BSD_3Clause
 #
 # >> USAGE
 # This script gets executed when running matjaz_dotfiles_installer.sh.
-# You can execute it also as stand-alone anywhere on the system. There are no
-# parameters.
+# You can execute it also as stand-alone anywhere on a Debian or Ubuntu
+# operative system. There are no parameters.
 #
 # >> WHAT IT DOES
 # Install packages (programs) for which the Matjaž's dotfiles are the
-# configuration files. It calls the system's package manager: currently for
-# OS X and Debian/Ubuntu only.
+# configuration files. It calls the system's package manager apt-get.
 # ------------------------------------------------------------------------------
 
+prompt="[ DOTFILES ]>"
 
-# Find the current operating system
-echo "Installing some packages to be set with the dotfiles."
-case $(uname) in
-    'Darwin')
-        echo "OS X detected."
-        # Install Homebrew if not already installed
-        type brew 2>&1 > /dev/null
-        if [ $? != 0 ]; then
-            echo "Installing Homebrew, will be used to install the packages."
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        else
-            echo "Found Homebrew, skipping installation."
-        fi
-        echo "Updating Homebrew local repository."
-        brew update
-        echo "Installing the packages."
-        brew install brew-cask emacs git git-flow htop-osx midnight-commander python3 sqlite wget
-        echo "Performing some cleaning."
-        brew cleanup --force -s   
-        brew cask cleanup
-        ;;
-    'Linux')
-        if [ -f /etc/debian_version ] ; then
-            echo "Debian/Ubuntu detected."
-            echo "Updating apt-get. It may ask you for the root password."
-            sudo apt-get update
-            echo "Installing the packages."
-            sudo apt-get -y install build-essential git git-flow htop mc emacs zsh sqlite3 python3-pip coreutils findutils moreutils screen gpg
-            echo "Performing some cleaning."
-            sudo apt-get autoremove
-            sudo apt-get clean
-            sudo apt-get autoclean
-        else
-            echo "Not implemented for this Linux operative system.
-Please update this script $0"
-            exit 100
-        fi
-        ;;
-    *)
-        echo "Not implemented for this operative system.
-Please update this script $0"
+
+# Terminates the script if the current operative system is not Debian or Ubuntu
+function verify_operative_system() {
+    if [ $(uname) != "Linux" ] ; then
+        echo "$prompt The current operative system is not Linux; terminating."
+        exit 101
+    elif [ -f /etc/debian_version ] ; then
+        echo "$prompt The current operative system is not a Debian or Ubuntu; terminating."
         exit 100
-        ;;
-esac
+    fi
+}
+
+
+# Performs an initial OS check
+verify_operative_system
+
+
+# Install the useful packages that are configured by the dotfiles
+echo "$prompt Updating apt-get. It may ask you for the root password."
+sudo apt-get update
+echo "$prompt Installing the packages required by the dotfiles."
+sudo apt-get -y install build-essential git git-flow htop mc emacs zsh sqlite3 python3-pip coreutils findutils moreutils screen gpg
+
 
 # Install Oh My ZSH if not already installed
 if [ -d $HOME/.oh-my-zsh ]; then
-    echo "Oh My ZSH installation found, skipping install."
+    echo "$prompt Oh My ZSH installation found; skipping install."
 else
     # Download and run Oh My ZSH installer without letting it enter the zsh
     # so the script may continue. It's done by cutting off the "env zsh" line.
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed '/env zsh/d')"
-    echo "Setting zsh as default shell. It may ask you for the root password."
+    echo "$prompt Setting zsh as default shell. It may ask you for the root password."
     sudo chsh -s $(which zsh)
 fi
+
 
 # Install the live syntax highlighting for Oh My ZSH
 zshsyntax_dir="$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 if [ -d $zshsyntax_dir/.git ]; then
-    echo "Found zsh-syntax-highlighting plugin, updating it."
+    echo "$prompt Found zsh-syntax-highlighting plugin, updating it."
     cd $zshsyntax_dir
     git checkout master
     git pull
     git checkout -
 else
-    echo "Installing the zsh-syntax-highlighting plugin."
+    echo "$prompt Installing the zsh-syntax-highlighting plugin."
     git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $zshsyntax_dir || {
-        echo "An error occurred during the cloning of the zsh-syntax-highlighting repository. Please try running this script again."
+        echo "$prompt An error occurred during the cloning of the zsh-syntax-highlighting repository. Please try running this script again."
         exit 1
     }
 fi
