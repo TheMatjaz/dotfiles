42a947a71cfdc7bb864501cffd04c87bd791689f
Matjaž <dev@matjaz.it>
2016-08-17 17:22:31 +0200

    Activare un-tab shortcut on SHIFT+TAB

--- emacs_init.el
+++ emacs_init.el
@@ -172,18 +172,18 @@ yasnippet
 (electric-indent-mode 0)
 
 ; Shift+Tab (Backtab) removes indentation
-;; ;; (global-set-key (kbd "<S-tab>") 'un-indent-by-removing-4-spaces)
-;; ;; (defun un-indent-by-removing-4-spaces ()
-;; ;;   "remove 4 spaces from beginning of of line"
-;; ;;   (interactive)
-;; ;;   (save-excursion
-;; ;;     (save-match-data
-;; ;;       (beginning-of-line)
-;; ;;       ;; get rid of tabs at beginning of line
-;; ;;       (when (looking-at "^\\s-+")
-;; ;;         (untabify (match-beginning 0) (match-end 0)))
-;; ;;       (when (looking-at (concat "^" (make-string tab-width ?\ )))
-;; ;;         (replace-match "")))))
+(global-set-key (kbd "<S-tab>") 'un-indent-by-removing-4-spaces)
+(defun un-indent-by-removing-4-spaces ()
+"remove 4 spaces from beginning of of line"
+ (interactive)
+   (save-excursion
+     (save-match-data
+       (beginning-of-line)
+       ;; get rid of tabs at beginning of line
+       (when (looking-at "^\\s-+")
+         (untabify (match-beginning 0) (match-end 0)))
+       (when (looking-at (concat "^" (make-string tab-width ?\ )))
+         (replace-match "")))))
 
 (setq-default c-basic-offset 4 c-default-style "linux")
 ;(define-key c-mode-base-map (kbd "RETURN") 'newline-and-indent)
